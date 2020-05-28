ffb6d041e80c87ccc968198090bbe164f7e4fae3
bomorov <bomorov199@gmail.com>
2020-05-28 19:20:21 +0600

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/ImageController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: Image
+        HostelContext db = new HostelContext();
+        [HttpGet]
+        public ActionResult Add_Student()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add_Student(Student student)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(student.ImageFile.FileName);
+            string extension = Path.GetExtension(student.ImageFile.FileName);
+            fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+            student.ImagePath = "~/Image/" + fileName;
+            fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
+            student.ImageFile.SaveAs(fileName);
+            db.Students.Add(student);
+            db.SaveChanges();
+            ModelState.Clear();
+            return RedirectToAction("Student_List", "Image");
+        }
+
+
+        public ActionResult Student_List()
+        {
+            return View(db.Students.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            var student = db.Students.FirstOrDefault(s => s.Id == id);
+
+            return View(student);
+        }
+
+
+     
+    }
+}
---
+++ WebApplication1/Models/HostelContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class HostelContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    
+    }
+}
